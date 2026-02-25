924b267d2c746ea627145076979e36d25614fc2d
Milton-Peluffo <milton.peluffoc@gmail.com>
2026-02-24 22:34:29 -0500

    feat: add missing setializable anotation in ProfileDto

--- app/src/main/java/com/tomildev/room_login_compose/features/auth/data/remote/dto/ProfileDto.kt
+++ app/src/main/java/com/tomildev/room_login_compose/features/auth/data/remote/dto/ProfileDto.kt
@@ -1,7 +1,9 @@
 package com.tomildev.room_login_compose.features.auth.data.remote.dto
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ProfileDto(
     @SerialName("id") val id: String,
     @SerialName("display_name") val displayName: String
