71c7893e866e44ae0d6b1f3d580913d4f6f9887b
Milton-Peluffo <milton.peluffoc@gmail.com>
2026-03-03 18:15:20 -0500

    test(auth/register): add unit tests for ValidateName use case

---
+++ app/src/test/java/com/tomildev/room_login_compose/user/ValidateNameTest.kt
@@ -0,0 +1,78 @@
+package com.tomildev.room_login_compose.user
+
+import com.tomildev.room_login_compose.core.domain.model.user.UserValidationError
+import com.tomildev.room_login_compose.core.domain.model.user.UserValidationResult
+import com.tomildev.room_login_compose.core.domain.use_case.user.ValidateName
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ValidateNameTest {
+
+    val validateName = ValidateName()
+
+    @Test
+    fun `Name with special symbols returns error`(){
+
+        val name = "user-?@"
+        val result = validateName.execute(name)
+
+        assert(result is UserValidationResult.Error)
+        assertEquals(UserValidationError.InvalidName, (result as UserValidationResult.Error).error)
+    }
+
+    @Test
+    fun `Name with spaces returns error`(){
+
+        val name = " User "
+        val result = validateName.execute(name)
+
+        assert(result is UserValidationResult.Error)
+        assertEquals(UserValidationError.InvalidName, (result as UserValidationResult.Error).error)
+    }
+
+    @Test
+    fun `Name with fewer than 3 characters returns error`(){
+
+        val name = "Us"
+        val result = validateName.execute(name)
+
+        assert(result is UserValidationResult.Error)
+        assertEquals(UserValidationError.TooShortName, (result as UserValidationResult.Error).error)
+    }
+
+    @Test
+    fun `Name with underscores returns success`(){
+
+        val name = "user_"
+        val result = validateName.execute(name)
+
+        assert(result is UserValidationResult.Success)
+    }
+
+    @Test
+    fun `Name with numbers returns success`(){
+
+        val name = "user72"
+        val result = validateName.execute(name)
+
+        assert(result is UserValidationResult.Success)
+    }
+
+    @Test
+    fun `Name with uppercases returns success`(){
+
+        val name = "MyUser"
+        val result = validateName.execute(name)
+
+        assert(result is UserValidationResult.Success)
+    }
+
+    @Test
+    fun `Name with uppercases, numbers and underscore returns success`(){
+
+        val name = "MyUser_72"
+        val result = validateName.execute(name)
+
+        assert(result is UserValidationResult.Success)
+    }
+}
